b8b755afe182a766276fe6010a0881ae95c81121
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-08-11 14:07:43 +0900

    fix

--- runall.sh
+++ runall.sh
@@ -8,16 +8,22 @@ sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
 
 # aws
-mkdir ~/.aws
+sudo apt install awscli -y
 touch ~/.aws/credentials
 cp aws/config ~/.aws/
 
+# download kits
 ./getkits.sh
-sudo docker load -i icm-2020.1.0.215.0-docker.tar.gz
+mv *.gz ~/
+sudo docker load -i ~/icm-2020.1.0.215.0-docker.tar.gz
+
+# download key
+aws s3 cp s3://asia-pac-training-2019/iris.key ~/
 
 # for icm-utils
 sudo apt install jq -y
+cd ~
 git clone https://github.com/IRISMeister/icm-util.git
-cp ./iris.key　./icm-util/irislicense
-mv ./IRIS-2020.1.0.215.0-lnxubuntux64.tar.gz ./icm-util/kits
-
+cd icm-util
+cp ~/iris.key ./irislicense
+mv ~/IRIS-2020.1.0.215.0-lnxubuntux64.tar.gz ./kits
