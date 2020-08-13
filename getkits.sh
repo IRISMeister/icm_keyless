9596dc54ea9ae37be992d31d585a5b4f787bf694
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-08-13 14:33:13 +0900

    add rm cookie

--- getkits.sh
+++ getkits.sh
@@ -3,5 +3,6 @@
 wget -qO /dev/null --keep-session-cookies --save-cookies cookie --post-data="UserName=$WRC_USERNAME&Password=$WRC_PASSWORD" 'https://login.intersystems.com/login/SSO.UI.Login.cls?referrer=https%253A//wrc.intersystems.com/wrc/login.csp' 
 wget --secure-protocol=TLSv1_2 -O $kit --load-cookies cookie "https://wrc.intersystems.com/wrc/WRC.StreamServer.cls?FILE=/wrc/Live/ServerKits/$kit"
 wget --secure-protocol=TLSv1_2 -O $icmkit --load-cookies cookie "https://wrc.intersystems.com/wrc/WRC.StreamServer.cls?FILE=/wrc/Live/Containers/$icmkit"
+rm -f cookie
 exit
 
