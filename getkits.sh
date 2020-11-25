2462bd52f0ac7f7623480f852d6784a85679f5bd
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2020-11-25 10:56:35 +0900

    add tests

--- getkits.sh
+++ getkits.sh
@@ -5,8 +5,12 @@ if [ -z "$WRC_PASSWORD" ]; then
     exit
 fi
 wget -qO /dev/null --keep-session-cookies --save-cookies cookie --post-data="UserName=$WRC_USERNAME&Password=$WRC_PASSWORD" 'https://login.intersystems.com/login/SSO.UI.Login.cls?referrer=https%253A//wrc.intersystems.com/wrc/login.csp' 
-wget --secure-protocol=TLSv1_2 -O $kit --load-cookies cookie "https://wrc.intersystems.com/wrc/WRC.StreamServer.cls?FILE=/wrc/Live/ServerKits/$kit"
-wget --secure-protocol=TLSv1_2 -O $icmkit --load-cookies cookie "https://wrc.intersystems.com/wrc/WRC.StreamServer.cls?FILE=/wrc/Live/Containers/$icmkit"
+if [ -n "$kit" ]; then
+    wget --secure-protocol=TLSv1_2 -O $kit --load-cookies cookie "https://wrc.intersystems.com/wrc/WRC.StreamServer.cls?FILE=/wrc/Live/ServerKits/$kit"
+fi
+if [ -n "$icmkit" ]; then
+    wget --secure-protocol=TLSv1_2 -O $icmkit --load-cookies cookie "https://wrc.intersystems.com/wrc/WRC.StreamServer.cls?FILE=/wrc/Live/Containers/$icmkit"
+fi
 rm -f cookie
 exit
 
